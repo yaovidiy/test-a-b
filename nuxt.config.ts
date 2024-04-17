e38f8c9ded35fd74ceeca22b27c3e32e65267563
Yaroslav Ovdii <yaovdiy@gmail.com>
2024-04-17 18:29:52 +0300

    added images/svg and tailwind config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: {
+        dir: 'auto',
+        lang: 'uk'
+      },
+      link: [{ rel: 'icon', type: 'image/png', href: '/assets/images/logo.png' }]
+    }
+  },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
   devtools: { enabled: true }
 })
